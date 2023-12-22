e762eefbf7a14e2c65afb603d37fe6e5ce757147
zouhang <1102639563@qq.com>
2023-12-22 21:51:04 +0800

    chore: update logic

--- packages/gemini-ai/src/index.tsx
+++ packages/gemini-ai/src/index.tsx
@@ -17,22 +17,34 @@ app.get('/', (c) => {
   return c.html(
     <html>
       <head>
-        <title>Hang AI</title>
-        <link rel="stylesheet" href="/static/css/index.css" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/github-markdown-css@5.5.0/github-markdown.css" />
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+        <title>Agent. Hang</title>
         <link
+          href="https://fonts.googleapis.com/css2?family=Figtree:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/github-markdown-css@5.5.0/github-markdown-light.css"
         />
+        <link rel="stylesheet" href="/static/css/uno.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/github-markdown-css@5.5.0/github-markdown.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/github-markdown-css@5.5.0/github-markdown-dark.css" />
       </head>
-      <body>
-        <div>
-          <h1>Gemini AI</h1>
-          <input id="q" />
-          <button>send</button>
+      <body class="dark flex items-center justify-center bg-#101010 font-[Figtree]">
+        <div class="relative flex flex-col gap-4 mx-auto md:w-180 lt-md:w-90% h-140 py-4 px-6 border-1 border-solid border-[#383838] bg-[#252525] rounded-4 overflow-hidden">
+          <div class="text-xl font-500 text-white">Agent. Hang</div>
+          <div class="markdown-body content relative p-4 flex-1 overflow-y-auto scroll-smooth rounded-2 scrollbar scrollbar-rounded scrollbar-w-0"></div>
+          <div class="flex p-2 justify-center items-end gap-4 self-stretch rounded-2 border-1 border-solid border-[#383838] bg-[#222]">
+            <textarea
+              id="q"
+              class="resize-none p-0 min-h-8 lh-8 flex-1 bg-transparent border-none outline-none text-sm text-[#A2A2A2]"
+              rows={1}
+              autoComplete="off"
+              placeholder=""
+            />
+            <button class="h-8 flex px-4 justify-center items-center rounded-1.5 text-3.5 font-500 cursor-pointer border-none outline-none bg-gradient-linear bg-gradient-[90deg,rgba(37,174,106,0.60)_0%,rgba(104,217,75,0.60)_100%] bg-transparent text-white">
+              send
+            </button>
+          </div>
         </div>
-
-        <div class="markdown-body content"></div>
       </body>
       <script type="module" src="/static/js/chat.js" />
     </html>
