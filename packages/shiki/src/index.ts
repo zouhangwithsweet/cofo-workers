70c7dec5bb5e6afaa6459287c531f5c9eafc80ca
zouhang <1102639563@qq.com>
2024-05-26 20:06:31 +0800

    chore: update shiki

--- packages/shiki/src/index.ts
+++ packages/shiki/src/index.ts
@@ -4,21 +4,14 @@ import { getHighlighterCore, loadWasm } from 'shiki/core'
 // directly import the theme and language modules, only the ones you imported will be bundled.
 import nord from 'shiki/themes/nord.mjs'
 
+await loadWasm(import('shiki/onig.wasm'))
+
 const app = new Hono()
 
 app.get('/', async (c) => {
   try {
-    const res = import('shiki/onig.wasm')
-    console.log(res)
-    await loadWasm(res)
-
     const highlighter = await getHighlighterCore({
-      themes: [
-        // instead of strings, you need to pass the imported module
-        nord,
-        // or a dynamic import if you want to do chunk splitting
-        import('shiki/themes/material-theme-ocean.mjs'),
-      ],
+      themes: [],
       langs: [
         import('shiki/langs/javascript.mjs'),
         // shiki will try to interop the module with the default export
@@ -29,11 +22,16 @@ app.get('/', async (c) => {
     // optionally, load themes and languages after creation
     await highlighter.loadTheme(import('shiki/themes/vitesse-light.mjs'))
 
-    const code = highlighter.codeToHtml('const a = 1', {
-      lang: 'javascript',
-      theme: 'material-theme-ocean',
+    const code = highlighter.codeToHtml(`.text {
+  width: 414px;
+  height: 896px;
+  border-radius: 32px;
+  background: #F9F9F9;
+}`, {
+      lang: 'css',
+      theme: 'vitesse-light',
     })
-    return c.text('Hello Hono!')
+    return c.html(code)
   } catch (error: any) {
     return c.text(JSON.stringify(error.message))
   }
