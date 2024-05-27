d0d7d06d8b9e2791e071e31f061b6d6309446332
zouhang <zouhang@baichuan-inc.com>
2024-05-27 11:12:26 +0800

    chore: update api query

--- packages/shiki/src/index.ts
+++ packages/shiki/src/index.ts
@@ -1,8 +1,6 @@
 import { Hono } from 'hono'
 // `shiki/core` entry does not include any themes or languages or the wasm binary.
 import { getHighlighterCore, loadWasm } from 'shiki/core'
-// directly import the theme and language modules, only the ones you imported will be bundled.
-import nord from 'shiki/themes/nord.mjs'
 
 await loadWasm(import('shiki/onig.wasm'))
 
@@ -22,12 +20,13 @@ app.get('/', async (c) => {
     // optionally, load themes and languages after creation
     await highlighter.loadTheme(import('shiki/themes/vitesse-light.mjs'))
 
-    const code = highlighter.codeToHtml(`.text {
-  width: 414px;
-  height: 896px;
-  border-radius: 32px;
-  background: #F9F9F9;
-}`, {
+    const queryCode = c.req.query('code')
+
+    if (!queryCode) {
+      return c.text('Please provide a code query param')
+    }
+
+    const code = highlighter.codeToHtml(queryCode, {
       lang: 'css',
       theme: 'vitesse-light',
     })
